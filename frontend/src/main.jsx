89284c8bcf28b3c03800988787b0b91dc011de6b
Thiago Augusto <luizthiagoaugusto@gmail.com>
2026-08-03 14:44:14 -0300

    fix(draft): register DraftProvider globally

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { DraftProvider } from './components/commom/draft/DraftContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DraftProvider>
+      <App />
+    </DraftProvider>
   </React.StrictMode>,
 )
